ed3e85e6fc1681ad80501ee0e2045df44d626cfc
AzuraAce <oliverjjst@proton.me>
2026-06-01 16:08:32 +0200

    changed status bar to be on bottom

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
+static int topbar             = 0;        /* 0 means bottom bar */
 static char font[]            = "monospace:size=10";
 static char dmenufont[]       = "monospace:size=10";
 static const char *fonts[]          = { font };
